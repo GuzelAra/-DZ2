84983f269faea0bead8b8c09ee268bb9a015cd5b
Guzel Elanskaya <guzelarazova@yandex.ru>
2022-11-17 17:37:51 +0300

    added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number=Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (number<100)
+    {Console.WriteLine("третьей цифры не сущ");}
+if (number>99&&number<1000)
+    {Console.WriteLine($"{number%10}");}
+if (number>999)
+    {while (number>999)
+    { number=number/10;}
+    Console.WriteLine($"{number%10}");}
+
+
